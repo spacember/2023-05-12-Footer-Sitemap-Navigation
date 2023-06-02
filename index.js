ab865c64d1c88a9a6ab0ca14d561fdca62449e94
Aditya Ramdonee (RAPP) <aditya.ramdonee@rapp.com>
2023-06-02 20:16:38 +0400

    fix: updateFooterSitemapNavigation working, refactored

--- index.js
+++ index.js
@@ -2,23 +2,17 @@ const contentful = require("contentful-management");
 const config = require('./config/config.json')
 const extract = require('./utils/extract')
 const log = require("./utils/log");
-const fs = require('fs')
+const fs = require('fs');
 
 const ACCESS_TOKEN = config.accessToken
 const SPACEID = config.space.spaceId
 const ENVIRONMENTID = config.space.environmentId
-const locale = config.space.locale
-
+const LOCALE = config.space.locale
+const FOOTERID = config.footerId
 const EXCEL = config.excel
 const OUTPUT_FILE = config.outputFile
 
-const date = new Date();
-
-/**
- * authenticates with contentful management api
- * @returns the specified environment
- */
-const connect = async () => {
+const getEnvironment = async () => {
     try {
         const client = contentful.createClient({ accessToken: ACCESS_TOKEN })
         const space = await client.getSpace(SPACEID)
@@ -28,24 +22,14 @@ const connect = async () => {
     }
 }
 
-/**
- * outputs the sitemap navigation object to the specified json file
- * @param {the path to the json file} path 
- * @param {the sitemap navigation object} object 
- */
 const outputObjectToFile = (path, object) => {
     try {
         fs.writeFileSync(path, JSON.stringify(object, null, 2), 'utf-8');
-    } catch (err) { log(`Error occured on outputSitemapNavToFile`) }
+    } catch (err) {
+        log(`Error occured on outputObjectToFile`)
+    }
 }
 
-/**
- * creates and publishes an entry of any type
- * @param {the environment} env 
- * @param {the content type of the entry} type 
- * @param {the entry} fields 
- * @returns the reference to the entry
- */
 const createEntry = async (env, type, fields) => {
     try {
         const entry = await env.createEntry(type, { fields: fields })
@@ -58,78 +42,83 @@ const createEntry = async (env, type, fields) => {
                 id: entry.sys.id
             }
         }
-        // log if entry published
-        // if (await entry.isPublished())
-        log(`DATE: ${date} \nPublished entry:: type: ${type}, name: ${entry.fields.title[locale]}, id: ${entry.sys.id}`)
+        log(`Published entry ${type}: name: ${entry.fields.title[LOCALE]} - id: ${entry.sys.id}`)
         return referenceToEntry
     } catch (err) {
-        console.log(err);
-        log(`Error occured on createEntry`)
+        log(`Error occured on createEntry. ${type}: name: ${fields.title[LOCALE]}`)
     }
 }
 
-/**
- * creates the entries for sitemap navigation. 
- * order: link -> categorie || link -> subCategorie -> categorieWithSubCategorie
- * after creating and publishing an entry, it is replaced with its system properties.
- * @param {the environment} env 
- * @param {the sitemap navigation object} sitemapNav 
- */
-const createEntries = async (env, sitemapNav) => {
-    const categories = sitemapNav.categories[locale]
-
-    for (let catIndex = 0; catIndex < categories.length; catIndex++) {
-        const categorie = categories[catIndex]
-        // if categorie
-        if ('links' in categorie) {
-            const links = categorie.links[locale]
-            // creates links for current categorie
-            for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
-                links[linkIndex] = await createEntry(env, 'link', links[linkIndex])
-            }
-            categories[catIndex] = createEntry(env, 'categorie', categorie)
-        }
-        // if categorieWithSubCategorie
-        else {
-            const subCategories = categorie.subCategories[locale]
-            // creates subCategories
-            for (let subCatIndex = 0; subCatIndex < subCategories.length; subCatIndex++) {
-
-                const subCategorie = subCategories[subCatIndex]
-                const links = subCategorie.links[locale]
-                // creates links for current subCategorie
-                for (let linkIndex = 0; linkIndex < links.length; linkIndex++) {
-                    // create a link entry. Replaces the link by its sys properties
-                    links[linkIndex] = await createEntry(env, 'link', links[linkIndex])
-                }
-                subCategories[subCatIndex] = await createEntry(env, 'subCategorie', subCategorie)
-            }
-            categories[catIndex] = await createEntry(env, 'categorieWithSubCategorie', categorie)
-        }
+const createLinks = async (env, links) => {
+    for (let index = 0; index < links.length; index++) {
+        links[index] = await createEntry(env, 'link', links[index])
     }
 }
 
-/**
- * creates field sitemap navigation on footer
- * under construction !
- * @param {the environment} env 
- * @param {field sitemap navigation} sitemapNav 
- */
-async function createFieldSitemapNav(env, sitemapNav) { }
+const createCategorie = async (env, categorie) => {
+    const links = categorie.links[LOCALE]
+    await createLinks(env, links)
+    categorie = await createEntry(env, 'categorie', categorie)
+    return categorie
+}
+
+const createSubCategorie = async (env, subCategorie) => {
+    const links = subCategorie.links[LOCALE]
+    await createLinks(env, links)
+    subCategorie = await createEntry(env, 'subCategorie', subCategorie)
+    return subCategorie
+}
+
+const createCategorieWithSubCategories = async (env, categorie) => {
+    const subCategories = categorie.subCategories[LOCALE]
+    for (let index = 0; index < subCategories.length; index++) {
+        subCategories[index] = await createSubCategorie(env, subCategories[index])
+    }
+    categorie = await createEntry(env, 'categorieWithSubCategorie', categorie)
+    return categorie
+}
+
+const createCategories = async (env, sitemapNav) => {
+    const categories = sitemapNav.categories[LOCALE]
+    for (let index = 0; index < categories.length; index++) {
+        // if categorieWithSubCategories
+        if ('subCategories' in categories[index])
+            categories[index] = await createCategorieWithSubCategories(env, categories[index])
+        else
+            categories[index] = await createCategorie(env, categories[index])
+    }
+}
+
+// updates references (categories || categoriesWithSubCategories) for field sitemap navigation 
+const updateFooterSitemapNavigation = async (env, categories) => {
+    try {
+        const footer = await env.getEntry(FOOTERID)
+        footer.fields.sitemapNavigation = categories
+        await footer.update()
+        const draft = await env.getEntry(FOOTERID)
+        await draft.publish()
+        log(`Updated footer: ${FOOTERID} field: sitemap navigation`)
+    }
+    catch (err) {
+        log(`Error occured on createFieldSitemapNav. Field name: sitemap navigation`)
+    }
+}
 
 /**
  * main function
- * this script creates a sitemap navigation field with entries on footer, using an excel as input
+ * extracts the sitemap navigation (which is a field on footer) from excel
+ * creates the requested categories (entries) 
+ * adds the entries to the specified footer's sitemap navigation field
  */
 (async () => {
-    // the environment
-    const env = await connect();
-    // the sitemapNav object
-    const sitemapNav = await extract(EXCEL)
-    // outputs sitemapNav to the specified json file
-    outputObjectToFile(OUTPUT_FILE, sitemapNav)
-    // creates the required entries for sitemap navigation field.
-    await createEntries(env, sitemapNav)
-    // creates field sitemap navigation on footer
-    // await createFieldSitemapNav(env, sitemapNav)
+    try {
+        const env = await getEnvironment();
+        const sitemapNav = await extract(EXCEL) // should return categories instead
+        // outputs sitemapNav to the specified json file
+        outputObjectToFile(OUTPUT_FILE, sitemapNav)
+        await createCategories(env, sitemapNav)
+        await updateFooterSitemapNavigation(env, sitemapNav.categories)
+    } catch (err) {
+        log(`Error occured on main`)
+    }
 })()
